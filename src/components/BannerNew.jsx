072addd18b685f77bf94bc00a6bf61d4db7ac41b
¨mago¨ <martingramajo08@gmail.com>
2023-12-07 06:53:24 -0300

    quito el texto del banner y agrego el pixel

--- src/components/BannerNew.jsx
+++ src/components/BannerNew.jsx
@@ -5,14 +5,14 @@ const BannerNew = () => {
   return (
     <div className="contenedor-imagen">
       <Image src={banner} fluid alt="React-Bootstrap Image" />
-      <div className="overlay">
+      {/* <div className="overlay">
         <div className="text">
           <h6 className="texto-banner">
             Make It <br /> Happen{" "}
           </h6>
           <p className="sub-texto-banner">Encuentra Tu Hogar</p>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
